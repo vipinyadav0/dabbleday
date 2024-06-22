790ea3dd6365f11350f20eea11c3948f4434a249
Vipin Yadav <vyvipinyadav998@gmail.com>
2024-06-23 01:50:45 +0530

    build issue fixes

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { deleteInvoice } from '@/app/lib/actions';
@@ -31,11 +29,13 @@ export function DeleteInvoice({ id }: { id: string }) {
   const deleteInvoiceWithId = deleteInvoice.bind(null, id);
 
   return (
-    <form action={deleteInvoiceWithId}>
-      <button className="rounded-md border p-2 hover:bg-gray-100">
-        <span className="sr-only">Delete</span>
-        <TrashIcon className="w-5" />
-      </button>
-    </form>
+    // <form action={deleteInvoiceWithId}>
+    //   <button className="rounded-md border p-2 hover:bg-gray-100">
+    //     <span className="sr-only">Delete</span>
+    //     <TrashIcon className="w-5" />
+    //   </button>
+    // </form>
+    <>
+    </>
   );
 }
