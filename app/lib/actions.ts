027eef19ebcafde23f7711d0433b57b53c6bc8f9
Vipin Yadav <vyvipinyadav998@gmail.com>
2024-06-23 01:42:46 +0530

    build issue fixes

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -127,7 +127,7 @@ export async function authenticate(
     // await signIn('credentials', formData);
   } catch (error) {
     if (error instanceof AuthError) {
-      switch (error.type) {
+      switch (error) {
         case 'CredentialsSignin':
           return 'Invalid credentials.';
         default:
